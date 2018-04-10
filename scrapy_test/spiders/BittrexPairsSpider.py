655d6aef918f37afd58c4731b9ab514ac0a090c0
eaa13 <47704086@qq.com>
2018-04-10 16:20:06 +0800

    add a file

---
+++ scrapy_test/spiders/BittrexPairsSpider.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from crypto_data_collection.items import BittrexPairsItem
+
+
+class BinancePairsSpider(scrapy.Spider):
+    name = 'bittrex-pairs'
+    allowed_domains = ['bittrex.com']
+    start_urls = ['https://bittrex.com/api/v2.0/pub/Markets/GetMarketSummaries']
+    connection = None
+
+    def parse(self, response):
+        if response.status == 200:
+            response = json.loads(response.body.decode('utf-8'))
+            for i in response['result']:
+                item = BittrexPairsItem()
+                item['base_currency'] = i['Market']['BaseCurrency']
+                item['quote_currency'] = i['Market']['MarketCurrency']
+                item['base_currency_name'] = i['Market']['BaseCurrencyLong']
+                item['quote_currency_name'] = i['Market']['MarketCurrencyLong']
+                yield item
+
